1959a9fe61bc7ec99c28881cdd6e60f69fee0d58
JosCha94 <72150891+JosCha94@users.noreply.github.com>
2021-11-23 10:22:41 -0500

    imgs y codigo para nueva parte de paginicio del site de AQParking

--- resources/views/AQParkingSite/main.blade.php
+++ resources/views/AQParkingSite/main.blade.php
@@ -6,9 +6,9 @@
 
 @section('content')
 <!-- BODY -->
-<div class="container my-5 py-5">
-    <div class="row py-5 my-xxl-2">
-        <div class="col-12 col-sm-6 d-flex justify-content-center align-items-center">
+<div class="container mb-5">
+    <div class="row">
+        <div class="col-12 col-sm-6 d-flex align-items-center justify-content-center">
             <div>
                 <h2 class="mt-5 mt-sm-0">Encuentra estacionamiento en segundos </h2>
                 <p>Ahorra tiempo y dinero</p>
@@ -21,9 +21,30 @@
                 </form>
             </div>
         </div>
-        <div class="col-12 col-sm-6 d-flex justify-content-center align-items-center">
+        <div class="col-12 col-sm-6  d-flex justify-content-center">
             <img class="img-fluid" src="img/City_driver.gif" title="imagen de carro" alt="imagen de carro">
         </div>
     </div>
+    <h2 class="d-none d-lg-block mt-3">¿Por qué usar nuestro servicio?</h2>
+        <div class="row beneficios mt-3">
+            <div class="col-md-4 d-none d-lg-block benef ">
+                <div class="col-lg-6">
+                    <img src="img/tiempo.png" alt="" class="img-fluid bene-ico">
+                    <p>Ahorra todo ese tiempo que gastas buscando una playa de estacionamiento libre</p>
+                </div>
+            </div>
+            <div class="col-md-4 d-none d-lg-block benef">
+                <div class="col-lg-6">
+                    <img src="img/gasolina2.png" alt="" class="img-fluid bene-ico">
+                    <p>No desperdicies combustible dando vueltas mientras buscas un lugar para estacionar</p>
+                </div>
+            </div>
+            <div class="col-md-4 d-none d-lg-block benef">
+                <div class="col-lg-6">
+                    <img src="img/clientes.png" alt="" class="img-fluid bene-ico">
+                    <p>Consigue nuevos clientes y haz que más gente conozca tu negocio</p>
+                </div>
+            </div>
+        </div>
 </div>
 @endsection
