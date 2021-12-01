60c5c8c569240e6aef944dc9882dd0b887f240cc
JosCha94 <72150891+JosCha94@users.noreply.github.com>
2021-12-01 11:56:12 -0500

    Add controls usuario , estacionamiento, reserva

---
+++ app/Http/Controllers/AQParkingSite/UsuarioAQParkingController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers\AQParkingSite;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class UsuarioAQParkingController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        return view('AQParkingSite.Index.principal');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function restore()
+    {
+        //
+        return view('AQParkingSite.Usuario.recuperacion-cuenta');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+
+    public function show()
+    // public function show($id)
+    {
+        //
+        return view('AQParkingSite.Usuario.cuenta-usuario');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
